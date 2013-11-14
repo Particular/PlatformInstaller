0dab217bd36909cfef504aff2d0881d5cba9adff
Bogdan Mitrache <mitrache.bogdan22@gmail.com>
2013-11-15 01:24:19 +0200

    More fine tunning on the new PlatformInstaller. Separated JS code in methods.

---
+++ installer/utils.js
@@ -0,0 +1,38 @@
+/*
+ * Method used to set installer properties based user selection
+ * from the HTML view.
+ *
+ * example of call:  SelectProduct("#controlId", "CONTROL_PROP");
+ */
+
+function SelectProduct(aProdId, aProdProperty)
+{
+    $(aProdId).click(function() {
+
+    if($(this).is(":checked"))
+    {
+        external.MsiSetProperty(aProdProperty, 'set');
+    }
+    else
+    {
+            // delete property
+        external.MsiSetProperty(aProdProperty, '[~]');
+    }
+
+  });
+}
+
+/*
+ * Method used to tick a checknox based on the value of a property.
+ *
+ * example of call:  TickCheckbox("#checkboId", "CHECKBOX_PROP");
+ */
+
+function TickCheckbox(aProdId, aProdProperty)
+{
+    if (external.MsiGetProperty(aProdProperty) == "set")
+    {
+        //alert( external.MsiGetProperty(aProdProperty) );
+        $(aProdId).prop('checked', true);
+    }
+}

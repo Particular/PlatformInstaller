84fa067e3e3be99dbe159f7d9c8267e0f82e0c12
Simon Cropp <simon.cropp@gmail.com>
2014-02-18 22:11:40 +1100

    add ps version checker

---
+++ src/PlatformInstaller.Tests/Chocolatey/PowerShellVersionTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+
+[TestFixture]
+public class PowerShellVersionTests
+{
+    [Test]
+    public void Initialise()
+    {
+        var packageInstaller = new PowerShellVersion();
+        packageInstaller.Initialise();
+        Debug.WriteLine(packageInstaller.Version);
+    }
+
+}
---
+++ src/PlatformInstaller/Chocolatey/PowerShellVersion.cs
@@ -0,0 +1,22 @@
+﻿public class PowerShellVersion
+{
+    public void Initialise()
+    {
+        //TODO: test on system with no PS
+        IsInstalled = true;
+
+        string output = null;
+        var runner = new PowerShellRunner("$PSVersionTable.PSVersion")
+        {
+            OutputDataReceived = x =>
+            {
+                output = x;
+            }
+        };
+        runner.Run().Wait();
+        Version = System.Version.Parse(output).Major;
+    }
+
+    public bool IsInstalled;
+    public int Version;
+}

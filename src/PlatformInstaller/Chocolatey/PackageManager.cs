901e616225522ffa8969c5da10dbe5829b9361cd
Simon Cropp <simon.cropp@gmail.com>
2014-02-18 20:27:56 +1100

    add install+uninstall of packages

---
+++ src/PlatformInstaller/Chocolatey/PackageManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public class PackageManager
+{
+    string packageName;
+
+    public Action<string> OutputDataReceived = x => { };
+    public Action<string> OutputErrorReceived = x => { };
+    public PackageManager(string packageName)
+    {
+        this.packageName = packageName;
+    }
+
+    public Task Install()
+    {
+        var runner = new PowerShellRunner("cinst " + packageName)
+        {
+            OutputDataReceived = OutputDataReceived,
+            OutputErrorReceived = OutputErrorReceived,
+        };
+        return runner.Run();
+    }
+
+    public Task Uninstall()
+    {
+        var runner = new PowerShellRunner("cuninst " + packageName)
+        {
+            OutputDataReceived = OutputDataReceived,
+            OutputErrorReceived = OutputErrorReceived,
+        };
+        return runner.Run();
+    }
+}

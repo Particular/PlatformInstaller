22264769d0be4aa34045cdbd89c202db49e315f4
Simon Cropp <simon.cropp@gmail.com>
2014-03-05 16:58:17 +1100

    wire in autofac

--- src/PlatformInstaller/AppBootstrapper.cs
+++ src/PlatformInstaller/AppBootstrapper.cs
@@ -1,8 +1,44 @@
-﻿using Caliburn.Micro;
+﻿using System;
+using System.Linq;
+using Autofac;
+using Caliburn.Micro;
 
 namespace PlatformInstaller
 {
     public class AppBootstrapper : Bootstrapper<MainViewModel>
     {
+        static IContainer Container;
+        protected override void Configure()
+        {
+            var builder = new ContainerBuilder();
+            builder.Register<IWindowManager>(c => new WindowManager()).InstancePerLifetimeScope();
+            
+            builder.RegisterAssemblyTypes(AssemblySource.Instance.ToArray())
+              .Where(type => type.Name.EndsWith("ViewModel"))
+              .AsSelf()
+              .InstancePerDependency();
+
+            builder.RegisterAssemblyTypes(AssemblySource.Instance.ToArray())
+              .Where(type => type.Name.EndsWith("View"))
+              .AsSelf()
+              .InstancePerDependency();
+            Container = builder.Build();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            object instance;
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                if (Container.TryResolve(service, out instance))
+                    return instance;
+            }
+            else
+            {
+                if (Container.TryResolveNamed(key, service, out instance))
+                    return instance;
+            }
+            throw new Exception(string.Format("Could not locate any instances of contract {0}.", key ?? service.Name));
+        }
     }
 }
--- src/PlatformInstaller/MainView.xaml.cs
+++ src/PlatformInstaller/MainView.xaml.cs
@@ -2,9 +2,6 @@
 
 namespace PlatformInstaller
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainView : Window
     {
         public MainView()

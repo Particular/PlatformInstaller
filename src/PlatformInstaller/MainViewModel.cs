f77a6c0c14534c3097fcbe6d49168d9e2da8a301
joaquinjares <joaquinjares@hotmail.com>
2014-03-10 15:47:53 -0300

    Coded close button

--- src/PlatformInstaller/MainViewModel.cs
+++ src/PlatformInstaller/MainViewModel.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using PropertyChanged;
 using System.Collections.Generic;
+using System.Windows;
 
 namespace PlatformInstaller
 {
@@ -39,77 +40,9 @@ namespace PlatformInstaller
             return products.Concat(products.SelectMany(e => Flatten(e.Children) ?? new List<IPackage>()));
         }
 
-        public async void InstallServiceInsight()
+        public void Close()
         {
-            await InstallPackage("ServiceInsight.install");
-        }
-        public bool CanInstallServiceInsight
-        {
-            get { return CanInstall; }
-        }
-
-        public async void InstallServiceControl()
-        {
-            await InstallPackage("ServiceControl.install");
-        }
-        public bool CanInstallServiceControl
-        {
-            get { return CanInstall; }
-        }
-
-        public async void InstallServicePulse()
-        {
-            await InstallPackage("ServicePulse.install");
-        }
-        public bool CanInstallServicePulse
-        {
-            get { return CanInstall; }
-        }
-
-        public async void InstallServiceMatrix()
-        {
-            await InstallPackage("ServiceMatrix.install");
-        }
-        public bool CanInstallServiceMatrix
-        {
-            get { return CanInstall; }
-        }
-
-        public async void InstallMsmq()
-        {
-            await InstallPackage("NServicebus.Msmq.install");
-        }
-
-        public bool CanInstallMsmq
-        {
-            get { return CanInstall; }
-        }
-
-        public async void InstallDtc()
-        {
-            await InstallPackage("NServicebus.Dtc.install");
-        }
-        public bool CanInstallDtc
-        {
-            get { return CanInstall; }
-        }
-        public async void InstallPerfCounters()
-        {
-            await InstallPackage("NServicebus.PerfCounters.install");
-        }
-
-        public bool CanInstallPerfCounters
-        {
-            get { return CanInstall; }
-        }
-
-        public async void InstallRavenDb()
-        {
-            await InstallPackage("RavenDB");
-        }
-        public bool CanInstallRavenDb
-        {
-            get { return CanInstall; }
+            Application.Current.Shutdown();
         }
 
         async Task InstallPackage(string packageName)

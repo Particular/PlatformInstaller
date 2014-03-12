c24e2c30a3923ce38841d41df6c5d397adebf34f
Andreas Öhlund <andreas.ohlund@particular.net>
2014-03-12 18:12:26 +0100

    Adding callback to particular.net for analytics purposes

---
+++ src/PlatformInstaller.Tests/NewUserDetecterTests.cs
@@ -0,0 +1,64 @@
+﻿namespace PlatformInstaller.Tests
+{
+    using System;
+    using Microsoft.Win32;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class NewUserDetecterTests
+    {
+        [Test]
+        public void Should_classify_existing_nservicebus_key_as_a_existing_user()
+        {
+            CreateSubKey("NServiceBus");
+
+            Assert.False(detecter.IsNewUser());
+        }
+
+        void CreateSubKey(string keyName)
+        {
+            using (regRoot.CreateSubKey(keyName))
+            { }
+        }
+
+        [Test]
+        public void Should_classify_existing_particular_key_as_a_existing_user()
+        {
+            CreateSubKey("ParticularSoftware");
+
+            Assert.False(detecter.IsNewUser());
+        }
+
+
+        [Test]
+        public void Should_classify_as_new_user_when_both_nsb_and_platform_key_is_missing()
+        {
+            Assert.True(detecter.IsNewUser());
+        }
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            subKey = Guid.NewGuid().ToString();
+            regRoot = Registry.CurrentUser.CreateSubKey(subKey);
+
+            detecter = new NewUserDetecter(regRoot);
+        }
+
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            regRoot.Close();
+            regRoot.Dispose();
+            Registry.CurrentUser.DeleteSubKeyTree(subKey);
+        }
+
+
+        NewUserDetecter detecter;
+        RegistryKey regRoot;
+        string subKey;
+    }
+}
---
+++ src/PlatformInstaller/NewUserDetecter.cs
@@ -0,0 +1,30 @@
+﻿namespace PlatformInstaller.Tests
+{
+    using System.Linq;
+    using Microsoft.Win32;
+
+    public class NewUserDetecter
+    {
+        readonly RegistryKey regKey;
+
+        public static NewUserDetecter Current
+        {
+            get
+            {
+                return new NewUserDetecter(Registry.CurrentUser);
+            }
+        }
+
+        public NewUserDetecter(RegistryKey regKey)
+        {
+            this.regKey = regKey;
+        }
+
+        public bool IsNewUser()
+        {
+            return regKey.GetSubKeyNames().All(subKey => 
+                subKey != "NServiceBus" &&
+                subKey != "ParticularSoftware");
+        }
+    }
+}

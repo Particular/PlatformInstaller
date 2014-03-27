ea9900843eac263da0fd7c67646d2cab67414532
Simon Cropp <simon.cropp@gmail.com>
2014-03-28 09:28:26 +1100

    redundant code

--- src/Test-PlatformInstaller/Program.cs
+++ src/Test-PlatformInstaller/Program.cs
@@ -5,7 +5,7 @@ class Program
     [STAThread]
     public static void Main()
     {
-        App app = new App();
+        var app = new App();
         app.InitializeComponent();
         app.Run();
 

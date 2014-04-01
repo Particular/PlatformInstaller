9f2a872d7675a59890f1fd5ae01bf220f59b80ce
Andreas Öhlund <andreas.ohlund@particular.net>
2014-04-01 12:27:00 +0200

    Now logging the version when we start

--- src/PlatformInstaller/App.xaml.cs
+++ src/PlatformInstaller/App.xaml.cs
@@ -1,9 +1,14 @@
-﻿public partial class App
+﻿using Serilog;
+
+public partial class App
 {
     public App()
     {
         Logging.Initialise();
         ExceptionHandler.Attach();
+
+        Log.Information(string.Format("Starting PlatformInstaller v{0}", PlatformInstaller.Version)); 
+        
         new AppBootstrapper().Start();
     }
 }
---
+++ src/PlatformInstaller/PlatformInstaller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+public class PlatformInstaller
+{
+    public static string Version
+    {
+        get
+        {
+            var type = typeof(PlatformInstaller);
+
+            if (!String.IsNullOrEmpty(type.Assembly.Location))
+            {
+                var fileVersion = FileVersionInfo.GetVersionInfo(type.Assembly.Location);
+
+                return new Version(fileVersion.FileMajorPart, fileVersion.FileMinorPart, fileVersion.FileBuildPart).ToString(3);
+            }
+
+            var customAttributes = type.Assembly.GetCustomAttributes(typeof(AssemblyFileVersionAttribute), false);
+
+            if (customAttributes.Length >= 1)
+            {
+                var fileVersion = (AssemblyFileVersionAttribute)customAttributes[0];
+                Version version;
+                if (System.Version.TryParse(fileVersion.Version, out version))
+                {
+                    return version.ToString(3);
+                }
+            }
+
+            return type.Assembly.GetName().Version.ToString(3);            
+        }
+    }
+
+}

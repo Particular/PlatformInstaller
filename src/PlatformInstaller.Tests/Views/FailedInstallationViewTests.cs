e229edd16e2980bce6983336a3ede70ab512d66a
Simon Cropp <simon.cropp@gmail.com>
2014-04-02 17:39:07 +1100

    fixes #55

---
+++ src/PlatformInstaller.Tests/Views/FailedInstallationViewTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Autofac;
+using NUnit.Framework;
+
+[TestFixture]
+public class FailedInstallationViewTests
+{
+    [Test]
+    [Explicit]
+    [RequiresSTA]
+    public void Show()
+    {
+        var failureReason = new NamedParameter("failureReason","The failure reason");
+        var failures = new NamedParameter("failures",new List<string>{"Error1", "Error2"});
+        ShellViewModel.StartModel = ContainerFactory.Container.Resolve<FailedInstallationViewModel>(failureReason, failures);
+        var app = new App();
+        app.InitializeComponent();
+        app.Run();
+    }
+
+    [Test]
+    [Explicit]
+    [RequiresSTA]
+    public void ScreenShot()
+    {
+        var failureReason = new NamedParameter("failureReason", "The failure reason");
+        var failures = new NamedParameter("failures", new List<string> { "Error1", "Error2" });
+        ContainerFactory.Container.Resolve<FailedInstallationViewModel>(failureReason, failures).TakeScreenShot();
+    }
+}

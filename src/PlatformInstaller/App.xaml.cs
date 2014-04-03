142462032215753d6f2853e2830c2e698537a292
Greg Bielleman <greg.bielleman@me.com>
2014-04-04 09:19:32 +1100

    Fixes #116

--- src/PlatformInstaller/App.xaml.cs
+++ src/PlatformInstaller/App.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System.Diagnostics;
+using System.Threading;
+using System.Windows;
 using Serilog;
 
 public partial class App
 {
+    static Mutex instanceLockMutex;
     static App()
     {
         if (!IsAdminChecker.IsAdministrator())
@@ -14,7 +17,17 @@ public partial class App
                 });
             DidRelaunchAsAdmin = true;
             return;
+        }    
+        
+        // Check if this is a second instance
+        bool mutexCreated;
+        instanceLockMutex = new Mutex(true, "particularplatforminstaller", out mutexCreated);
+        if (!mutexCreated)
+        {
+            MessageBox.Show("An instance of the Platform Installer is already running.", "");
+            Current.Shutdown();
         }
+
         Logging.Initialise();
         ExceptionHandler.Attach();
         Log.Information(string.Format("Starting PlatformInstaller v{0}", VersionFinder.GetVersion()));
@@ -28,4 +41,11 @@ public partial class App
         new AppBootstrapper().Start();
     }
 
+    protected override void OnExit(ExitEventArgs e)
+    {
+        if (instanceLockMutex != null)
+        {
+            instanceLockMutex.Dispose();
+        }
+    }
 }

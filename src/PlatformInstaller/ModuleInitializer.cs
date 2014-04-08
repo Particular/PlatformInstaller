682374dfad7cdd815418bc1f6429bb38e8755e07
Simon Cropp <simon.cropp@gmail.com>
2014-04-08 10:23:33 +1000

    dont module init in design time

--- src/PlatformInstaller/ModuleInitializer.cs
+++ src/PlatformInstaller/ModuleInitializer.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel;
+using System.Windows;
+
 public static class ModuleInitializer
 {
     public static void Initialize()
     {
+        if (DesignerProperties.GetIsInDesignMode(new DependencyObject()))
+        {
+            return;
+        }
         Logging.Initialise();
         ExceptionHandler.Attach();
     }

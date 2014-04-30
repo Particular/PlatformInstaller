b82148b42b79633e3fd8dcf5a4258cc40d69fe52
Greg Bielleman <greg.bielleman@gmail.com>
2014-04-30 19:39:44 +1000

    cleanup using

--- src/PlatformInstaller/Views/InstallingViewModel.cs
+++ src/PlatformInstaller/Views/InstallingViewModel.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Linq;
 using System.Management.Automation;
 using System.Threading.Tasks;

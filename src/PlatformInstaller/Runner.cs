0b617114c9fb5cee51364757cd1f4473032bc2b8
Greg Bielleman <greg.bielleman@gmail.com>
2014-05-09 11:16:32 +1000

    Tidy up

--- src/PlatformInstaller/Runner.cs
+++ src/PlatformInstaller/Runner.cs
@@ -7,34 +7,42 @@ using Autofac;
 using Caliburn.Micro;
 
 public static class Runner
-{
+{ 
     public static void Run()
     {
-        const int CancelledByUser = 1223;
         if (!IsAdminChecker.IsAdministrator())
         {
-            var processStartInfo = new ProcessStartInfo
-                {
-                    FileName = AssemblyLocation.ExeFileName,
-                    Verb = "runas"
-                };
-
-            try
-            {
-                using (Process.Start(processStartInfo))
-                {
-                }
-            }
-            catch (Win32Exception ex)
-            {
-                if (ex.NativeErrorCode != CancelledByUser)
-                {
-                    throw;
-                }
-            }
-            Environment.Exit(0);
+            RelaunchAsElevatedInstance(); 
         }
+        RunSingleInstance();
+    }
 
+    static void RelaunchAsElevatedInstance()
+    { 
+        const int CancelledByUser = 1223;
+        var processStartInfo = new ProcessStartInfo
+        {
+            FileName = AssemblyLocation.ExeFileName,
+            Verb = "runas"
+        };
+
+        try
+        {
+            using (Process.Start(processStartInfo)){}
+        }
+        catch (Win32Exception ex)
+        {
+            // if user presses no to UAC it will throw an exception
+            if (ex.NativeErrorCode != CancelledByUser)
+            {
+                throw;
+            }
+        }
+        Environment.Exit(0);
+    }
+
+    static void RunSingleInstance()
+    {
         // Check if this is a second instance
         bool mutexCreated;
         using (new Mutex(true, "particularPlatformInstaller", out mutexCreated))
@@ -44,7 +52,6 @@ public static class Runner
                 MessageBox.Show("An instance of the Platform Installer is already running.", "");
                 return;
             }
-
             InnerRun();
         }
     }

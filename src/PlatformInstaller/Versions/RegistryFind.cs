3fe457142266773988ba049fb53940d5586784f3
Simon Cropp <simon.cropp@gmail.com>
2015-08-06 11:55:46 +1000

    flatten RegistryFind

--- src/PlatformInstaller/Versions/RegistryFind.cs
+++ src/PlatformInstaller/Versions/RegistryFind.cs
@@ -27,23 +27,26 @@ public class RegistryFind
                 {
                     using (var subKey = key.OpenSubKey(subKeyName))
                     {
-                        if (subKey != null)
+                        if (subKey == null)
                         {
-                            var publisher = (string) subKey.GetValue("Publisher", string.Empty, RegistryValueOptions.None);
-                           
-                            if (publisher.StartsWith("Particular Software", StringComparison.OrdinalIgnoreCase))
-                            {  
-                                var installDate = (string) subKey.GetValue("InstallDate", null, RegistryValueOptions.None);
-                                var productName = (string) subKey.GetValue("DisplayName",string.Empty, RegistryValueOptions.None);
-
-                                if ((installDate != null) && (productName.IndexOf(product, StringComparison.OrdinalIgnoreCase) >= 0))
-                                {
-                                    var version = (string) subKey.GetValue("DisplayVersion",string.Empty, RegistryValueOptions.None);
-                                    SemanticVersion.TryParse(version, out versionFound);
-                                    return true;
-                                }
-                            }
+                            continue;
                         }
+                        var publisher = (string) subKey.GetValue("Publisher", string.Empty, RegistryValueOptions.None);
+
+                        if (!publisher.StartsWith("Particular Software", StringComparison.OrdinalIgnoreCase))
+                        {
+                            continue;
+                        }
+                        var installDate = (string) subKey.GetValue("InstallDate", null, RegistryValueOptions.None);
+                        var productName = (string) subKey.GetValue("DisplayName",string.Empty, RegistryValueOptions.None);
+
+                        if ((installDate == null) || (productName.IndexOf(product, StringComparison.OrdinalIgnoreCase) < 0))
+                        {
+                            continue;
+                        }
+                        var version = (string) subKey.GetValue("DisplayVersion",string.Empty, RegistryValueOptions.None);
+                        SemanticVersion.TryParse(version, out versionFound);
+                        return true;
                     }
                 }
             }

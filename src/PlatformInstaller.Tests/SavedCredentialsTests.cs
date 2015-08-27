7be2020dd631170d4913eb6fd927bb039d00bea6
Simon Cropp <simon.cropp@gmail.com>
2015-08-27 13:41:27 +1000

    dont throw for missing key

--- src/PlatformInstaller.Tests/SavedCredentialsTests.cs
+++ src/PlatformInstaller.Tests/SavedCredentialsTests.cs
@@ -22,6 +22,6 @@ public class SavedCredentialsTests
 
     static void DeleteCedentials()
     {
-        Registry.CurrentUser.DeleteSubKey(@"Software\Particular\PlatformInstaller\Credentials");
+        Registry.CurrentUser.DeleteSubKey(@"Software\Particular\PlatformInstaller\Credentials", false);
     }
 }

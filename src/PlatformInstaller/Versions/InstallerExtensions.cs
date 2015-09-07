9de00b377625821dad7c7678a92cb6707557ca2d
Simon Cropp <simon.cropp@gmail.com>
2015-09-07 11:04:55 +1000

    remove redundant catch

--- src/PlatformInstaller/Versions/InstallerExtensions.cs
+++ src/PlatformInstaller/Versions/InstallerExtensions.cs
@@ -1,34 +1,25 @@
-﻿using System;
-
-public static class InstallerExtensions
+﻿public static class InstallerExtensions
 {
     public static string ExeInstallerStatus(this IInstaller runner)
     {
-        try
-        {
-            var current = runner.CurrentVersion();
-            var latest = runner.LatestAvailableVersion();
+        var current = runner.CurrentVersion();
+        var latest = runner.LatestAvailableVersion();
 
-            if (latest == null)
-            {
-                return null;
-            }
-
-            if (current == null)
-            {
-                return string.Format("Install {0}", latest);
-            }
-
-            if (current != latest)
-            {
-                return string.Format("Upgrade {0} to {1}",current, latest);
-            }
-            return string.Format("Latest is installed - {0}", current);
-        }
-        catch (NotSupportedException)
+        if (latest == null)
         {
             return null;
         }
+
+        if (current == null)
+        {
+            return string.Format("Install {0}", latest);
+        }
+
+        if (current != latest)
+        {
+            return string.Format("Upgrade {0} to {1}", current, latest);
+        }
+        return string.Format("Latest is installed - {0}", current);
     }
 
     public static string VsixInstallerStatus(this IInstaller runner, string version)

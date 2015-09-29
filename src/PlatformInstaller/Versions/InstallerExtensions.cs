ad393a341e679d9ec7a1fee39759d3623943f8a1
indualagarsamy <indu.alagarsamy@particular.net>
2015-09-28 17:04:57 -0700

    Removed ServiceMatrix from PI.
    Related Issue: https://github.com/Particular/PlatformDevelopment/issues/454

--- src/PlatformInstaller/Versions/InstallerExtensions.cs
+++ src/PlatformInstaller/Versions/InstallerExtensions.cs
@@ -21,37 +21,5 @@
         }
         return string.Format("Latest is installed - {0}", current);
     }
-
-    public static string VsixInstallerStatus(this IInstaller runner, string version)
-    {
-        var latest = runner.LatestAvailableVersion();
-        var current = runner.CurrentVersion();
-
-        switch (version)
-        {
-            case VisualStudioVersions.VS2012:
-                if (!VisualStudioDetecter.VS2012Installed)
-                {
-                    return "Requires VS 2012";
-                }
-                break;
-            case VisualStudioVersions.VS2013:
-                if (!VisualStudioDetecter.VS2013Installed)
-                {
-                    return "Requires VS 2013";
-                }
-                break;
-        }
-        if (runner.Installed() && (latest == current))
-        {
-            return string.Format("Latest is installed - {0}", current);
-        }
-        if (runner.Installed() && (latest != current))
-        {
-            return string.Format("Update to {0} in Visual Studio", latest);
-        }
-        
-        return  (latest == null) ? "" : string.Format("Install {0}", latest);
-    }
 }
 
--- src/PlatformInstaller/VisualStudioVersions.cs
+++
@@ -1,5 +0,0 @@
-public static class VisualStudioVersions
-{
-    public const string VS2013 = "VS2013";
-    public const string VS2012 = "VS2012";
-}

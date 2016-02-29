560eb22a66c1ef29daf40f2a2730830475b8db77
Greg Bielleman <greg.bielleman@particular.net>
2016-02-29 17:59:46 +1100

    Add dotnet 4.5.2 version checker

---
+++ src/PlatformInstaller/DotnetVersionChecker.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Win32;
+
+public class DotnetVersionChecker
+{
+    private static bool Is452orLaterInstalled()
+    {
+        using (var ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\"))
+        {
+            if (ndpKey != null && ndpKey.GetValue("Release") != null)
+            {
+                int release;
+                if (int.TryParse((string)ndpKey.GetValue("Release"), out release))
+                {
+                    return release >= 379893;
+                }
+            }
+        }
+        return false;
+    }
+}

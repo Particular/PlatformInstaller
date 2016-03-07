667f69a461e1be4c6c937d7407fc0ae06f72fc2a
Simon Cropp <simon.cropp@gmail.com>
2016-03-08 08:55:42 +1100

    add test harnesses for the new views

---
+++ src/PlatformInstaller.Tests/Views/DotNetInstallCompleteViewTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class DotNetInstallCompleteViewTests
+{
+    [Test]
+    [Explicit]
+    [RequiresSTA]
+    public void Show()
+    {
+        var model = GetModel();
+        ViewTester.ShowView(model);
+    }
+
+
+    [Test]
+    [RequiresSTA]
+    [Explicit]
+    public void Screenshot()
+    {
+        var model = GetModel();
+        ViewTester.ScreenCapture(model);
+    }
+
+    static DotNetInstallCompleteViewModel GetModel()
+    {
+        return new DotNetInstallCompleteViewModel();
+    }
+}

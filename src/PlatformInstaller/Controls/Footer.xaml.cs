bc5fe3eeefef54c80902a72096b615ee0a21a68e
Greg Bielleman <greg.bielleman@particular.net>
2016-07-19 11:49:31 +1000

    Make styling closer to SC management UI

---
+++ src/PlatformInstaller/Controls/Footer.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+public partial class Footer
+{
+    public Footer()
+    {
+        InitializeComponent();
+        AppVersion.Text = $"Version: {Assembly.GetExecutingAssembly().GetName().Version}";
+    }
+}

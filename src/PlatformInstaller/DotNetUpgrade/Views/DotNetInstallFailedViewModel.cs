ed603c490bc4badd9d1d656ecd0c4e91b743cad0
Greg Bielleman <greg.bielleman@particular.net>
2017-03-10 16:21:43 +1100

    Fixed #200

--- src/PlatformInstaller/DotNetUpgrade/Views/DotNetInstallFailedViewModel.cs
+++ src/PlatformInstaller/DotNetUpgrade/Views/DotNetInstallFailedViewModel.cs
@@ -2,4 +2,19 @@ using Caliburn.Micro;
 
 public class DotNetInstallFailedViewModel : Screen
 {
+    IEventAggregator eventAggregator;
+
+    public DotNetInstallFailedViewModel()
+    {
+
+    }
+    public DotNetInstallFailedViewModel(IEventAggregator eventAggregator)
+    {
+        this.eventAggregator = eventAggregator;
+    }
+
+    public void Exit()
+    {
+        eventAggregator.Publish<ExitApplicationCommand>();
+    }
 }
